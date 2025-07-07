301528d426ee2625d4082e1c7177dab6d05b0ae2
Samir Patel <sp20042011@gmail.com>
2025-07-07 10:26:23 +0530

    Update MAx_Min_Array.py

--- Array/MAx_Min_Array.py
+++ Array/MAx_Min_Array.py
@@ -1,12 +1,13 @@
 def max_and_min(arr):
     max=0
-    min=0
+    min=float("inf")
     for i in range(len(arr)):
 
-        if arr[max]<arr[i]:
-            max=i
-        elif arr[min]>arr[i]:
-            min=i
+        if max<arr[i]:
+            max=arr[i]
+        
+        if min>arr[i]:
+            min=arr[i]
         
     print("Max number of list:",arr[max])
     print("Min number of list:",arr[min])
