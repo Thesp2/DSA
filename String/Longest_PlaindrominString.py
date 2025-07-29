2fe3138710fd17b1b5e5a25a668d470951af77a2
Samir Patel <sp20042011@gmail.com>
2025-07-29 23:55:12 +0530

    Add files via upload

---
+++ String/Longest_PlaindrominString.py
@@ -0,0 +1,24 @@
+def longestpalindrom(s):
+    res=""
+    reslen=0
+
+    for i in range(len(s)):
+        l,r=i,i
+        while l>=0 and r<len(s) and s[r]==s[l]:
+            if r-l+1>reslen:
+                res=s[l:r+1]
+                reslen=r-l+1
+            l-=1
+            r+=1
+        
+        l,r=i,i+1
+        while l>=0 and r<len(s) and s[r]==s[l]:
+            if r-l+1>reslen:
+                res=s[l:r+1]
+                reslen=r-l+1
+            l-=1
+            r+=1
+    return res
+
+s='abad'
+print(longestpalindrom(s))
