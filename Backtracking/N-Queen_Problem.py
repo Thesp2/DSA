0164e59ae7392512ffcab49290610aebe950fbef
Samir Patel <sp20042011@gmail.com>
2025-09-08 10:35:05 +0530

    Add files via upload

---
+++ Backtracking/N-Queen_Problem.py
@@ -0,0 +1,38 @@
+def nqueen(n):
+    ans=[]
+    board=[["."]*n for i in range(n)]
+    col=set()
+    posdig=set()#r+c
+    negdig=set()#r-c
+
+    def backtrack(r):
+        if r==n:
+            copy=["".join(row) for row in board]
+            ans.append(copy)
+            return 
+        
+        for c in range(n):
+            if c in col or r+c in posdig or r-c in negdig:
+                continue
+
+            col.add(c)
+            posdig.add(r+c)
+            negdig.add(r-c)
+            board[r][c]="Q"
+
+            backtrack(r+1)
+
+            col.remove(c)
+            posdig.remove(r+c)  
+            negdig.remove(r-c)
+            board[r][c]="."
+
+    backtrack(0)
+    return ans
+
+n=4
+solutions = nqueen(4)
+for sol in solutions:
+    for row in sol:
+        print(row)
+    print()
