9265c3d2ce6740df0a353bd3f6e49302a453a0c0
Samir Patel <sp20042011@gmail.com>
2025-09-15 21:10:42 +0530

    Add files via upload

---
+++ Stacks & Queues/Postfix.py
@@ -0,0 +1,23 @@
+def postfix(arr:list[str])->int:
+    stack=[]
+
+    for token in arr:
+
+        if token.lstrip('-').isdigit():
+            stack.append(int(token))
+
+        else:
+            val1=stack.pop()
+            val2=stack.pop()
+
+            if token=="+":
+                stack.append(val2+val1)
+            elif token=="-":
+                stack.append(val2-val1)
+            elif token=="*":
+                stack.append(val2*val1)
+            elif token=="/":
+                stack.append(val2/val1)
+    return stack.pop()
+arr = ["2", "3", "1", "*", "+"]
+print(postfix(arr))
